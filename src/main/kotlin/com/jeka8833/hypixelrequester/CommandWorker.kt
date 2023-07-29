f581709deb6aa4706b6346a84b1704450149c45f
Jeka8833 <eadercach@gmail.com>
2023-07-29 13:58:17 +0300

    Release 1.0.2.1

--- src/main/kotlin/com/jeka8833/hypixelrequester/CommandWorker.kt
+++ src/main/kotlin/com/jeka8833/hypixelrequester/CommandWorker.kt
@@ -10,7 +10,7 @@ import java.util.*
 
 @CommandLine.Command(
     name = "HypixelRequester",
-    version = ["HypixelRequester 1.0.2"],
+    version = ["HypixelRequester 1.0.2.1"],
     mixinStandardHelpOptions = true,
     description = ["A proxy client for requesting player data."]
 )
